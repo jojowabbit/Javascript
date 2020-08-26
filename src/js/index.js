cc08713c647ae58e68b3e5ee71fa3f09772176e6
jojowabbit <jojowabbit@gmail.com>
2020-08-26 12:24:24 +0800

    added model state & method

--- src/js/index.js
+++ src/js/index.js
@@ -3,13 +3,42 @@ import "@fortawesome/fontawesome-free/js/fontawesome";
 import "@fortawesome/fontawesome-free/js/solid";
 import "@fortawesome/fontawesome-free/js/regular";
 import "@fortawesome/fontawesome-free/js/brands";
+import { v4 as uuidv4 } from "uuid";
 
 // Main Style File
 import "../style/main.scss";
 
 // Model
 class Model {
-  constructor() {}
+  constructor() {
+    // State of model, prepopulated with array of objects
+    this.todos = [
+      { id: 1234, text: "todo 1", complete: false },
+      { id: 1234, text: "todo 1", complete: false },
+    ];
+  }
+  // Method Add, Edit, Delete, Toggle Completion
+  addTodo(todoText) {
+    const todo = { id: uuidv4(), text: todoText, complete: false };
+    this.todos.push(todo);
+  }
+  editTodo(id, updatedText) {
+    this.todos = this.todos.map((todo) =>
+      todo.id === id
+        ? { id: todo.id, text: updatedText, complete: todo.complete }
+        : todo
+    );
+  }
+  deleteTodo(id) {
+    this.todos = this.todos.filter((todo) => todo.id !== id);
+  }
+  toggleTodo(id) {
+    this.todos = this.todos.map((todo) =>
+      todo.id === id
+        ? { id: todo.id, text: todo.text, complete: !todo.complete }
+        : todo
+    );
+  }
 }
 
 // View
@@ -26,3 +55,5 @@ class Controller {
 }
 
 const app = new Controller(new Model(), new View());
+app.model.addTodo("third todo");
+console.log(app.model.todos);
