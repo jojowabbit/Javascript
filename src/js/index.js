2286c88927fe79beec5d7c4d6ce1d6684abb67ba
jojowabbit <jojowabbit@gmail.com>
2020-08-26 12:55:09 +0800

    added view state & method for create & get DOM element

--- src/js/index.js
+++ src/js/index.js
@@ -43,7 +43,42 @@ class Model {
 
 // View
 class View {
-  constructor() {}
+  constructor() {
+    // The root element
+    this.app = this.getElement("#root");
+    // Title of the app
+    this.title = this.createElement("h1");
+    this.title.textContent = "Todos";
+    // The Form
+    this.form = this.createElement("form");
+    // The Input with Type = text
+    this.input = this.createElement("input");
+    this.input.type = "text";
+    this.input.placeholder = "Add todo";
+    this.input.name = "todo";
+    // The Button
+    this.submitButton = this.createElement("button");
+    this.submitButton.textContent = "Submit";
+    // The UL of Todo List
+    this.todoList = this.createElement("ul", "todo-list");
+    // Append input & button to form
+    this.form.append(this.input, this.submitButton);
+    // Append title, form, ul to App
+    this.app.append(this.title, this.form, this.todoList);
+  }
+  // Create Element with optional CSS Class
+  createElement(tag, className) {
+    const element = document.createElement(tag);
+    if (className) {
+      element.classList.add(className);
+    }
+    return element;
+  }
+  // Retrieve element from DOM
+  getElement(selector) {
+    const element = document.querySelector(selector);
+    return element;
+  }
 }
 
 // Controller
