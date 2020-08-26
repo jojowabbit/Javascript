2d247b22293a568149d6d1215c96465fb2dc0fc8
jojowabbit <jojowabbit@gmail.com>
2020-08-26 14:17:21 +0800

    added view state & method

--- src/js/index.js
+++ src/js/index.js
@@ -66,6 +66,15 @@ class View {
     // Append title, form, ul to App
     this.app.append(this.title, this.form, this.todoList);
   }
+
+  // get value from input field
+  get _todoText() {
+    return this.input.value;
+  }
+  // reset input field
+  _resetInput() {
+    this.input.value = "";
+  }
   // Create Element with optional CSS Class
   createElement(tag, className) {
     const element = document.createElement(tag);
@@ -79,6 +88,50 @@ class View {
     const element = document.querySelector(selector);
     return element;
   }
+  // display Todo
+  displayTodos(todos) {
+    // Delete all nodes
+    while (this.todoList.firstChild) {
+      this.todoList.removeChild(this.todoList.firstChild);
+    }
+    // Show default message
+    if (todos.length === 0) {
+      const p = this.createElement("p");
+      p.textContent = "Nothing to show, add a task ?";
+      this.todoList.append(p);
+    } else {
+      // Create todo item node for each todo in the todos
+      todos.forEach((todo) => {
+        const li = this.createElement("li");
+        li.id = todo.id;
+        // Checkbox for each todo li
+        const checkbox = this.createElement("input");
+        checkbox.type = "checkbox";
+        checkbox.checked = todo.complete;
+        // The todo item will be in contenteditable span
+        const span = this.createElement("span");
+        span.contentEditable = true;
+        span.classList.add("editable");
+        // If todo is completed, add strikethrough todo.text to span
+        if (todo.complete) {
+          const strike = this.createElement("s");
+          strike.textContent = todo.text;
+          span.append(strike);
+        } else {
+          // otherwise display todo.text
+          span.textContent = todo.text;
+        }
+        // Delete button
+        const deleteButton = this.createElement("button", "delete");
+        deleteButton.textContent = "Delete";
+        // Append checkbox, span & button to li
+        li.append(checkbox, span, deleteButton);
+        // Append nodes to todo list
+        this.todoList.append(li);
+      });
+    }
+    console.log(todos);
+  }
 }
 
 // Controller
@@ -88,7 +141,6 @@ class Controller {
     this.view = view;
   }
 }
-
 const app = new Controller(new Model(), new View());
-app.model.addTodo("third todo");
+
 console.log(app.model.todos);
